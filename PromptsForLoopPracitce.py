913270e7102de70b3da83ad8a39eebb61edbff8d
Constance L Davis <71113829+conDavis@users.noreply.github.com>
2020-10-29 10:11:36 -0400

    Create PromptsForLoopPracitce.py

---
+++ PromptsForLoopPracitce.py
@@ -0,0 +1,144 @@
+#create a box with the letter X
+
+
+'''
+X X X X X X X X X
+X X X X X X X X X
+X X X X X X X X X
+X X X X X X X X X
+X X X X X X X X X
+X X X X X X X X X
+X X X X X X X X X
+X X X X X X X X X
+X X X X X X X X X
+'''
+row = "X X X X X X X X X"
+for x in range(9):
+  print(row)
+
+
+
+#make a box
+'''
+ ____________________
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|                    |
+|____________________|
+'''
+# you may use the following variables to help you
+top = " ____________________"
+middle = "|                    |"
+bottom = "|____________________|"
+
+print()
+
+for x in range(9):
+  if x == 0:
+    print(top)
+  elif x == 8:
+    print(bottom)
+  else:
+    print(middle)
+
+
+
+
+#create a triangle with smaller triangles!
+#you'll need this
+triangle = "▲"
+'''
+▲
+▲▲
+▲▲▲
+▲▲▲▲
+▲▲▲▲▲
+▲▲▲▲▲▲
+▲▲▲▲▲▲▲
+▲▲▲▲▲▲▲▲
+▲▲▲▲▲▲▲▲▲
+▲▲▲▲▲▲▲▲▲▲
+'''
+triangle = "▲"
+
+triLine = ""
+for x in range(10):
+  for y in range(x+1):
+    triLine = triLine + triangle
+    print(triLine)
+
+
+
+
+#make the triangle alternate between Xs and Os
+'''
+0
+01
+012
+0123
+01234
+012345
+0123456
+01234567
+012345678
+0123456789
+'''
+numbersLine = ""
+for x in range(10):
+  
+
+
+
+
+#make a heart box with a line through it
+'''
+  ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ 
+♥   ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ 
+♥ ♥   ♥ ♥ ♥ ♥ ♥ ♥ ♥ 
+♥ ♥ ♥   ♥ ♥ ♥ ♥ ♥ ♥ 
+♥ ♥ ♥ ♥   ♥ ♥ ♥ ♥ ♥ 
+♥ ♥ ♥ ♥ ♥   ♥ ♥ ♥ ♥ 
+♥ ♥ ♥ ♥ ♥ ♥   ♥ ♥ ♥ 
+♥ ♥ ♥ ♥ ♥ ♥ ♥   ♥ ♥ 
+♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥   ♥ 
+♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥   
+'''
+#you may use the following variables
+line = ""
+heart = "♥ "
+space = "  "
+
+
+#make the line go the other way (using the same variables from before)
+'''
+♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥   
+♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥   ♥ 
+♥ ♥ ♥ ♥ ♥ ♥ ♥   ♥ ♥ 
+♥ ♥ ♥ ♥ ♥ ♥   ♥ ♥ ♥ 
+♥ ♥ ♥ ♥ ♥   ♥ ♥ ♥ ♥ 
+♥ ♥ ♥ ♥   ♥ ♥ ♥ ♥ ♥ 
+♥ ♥ ♥   ♥ ♥ ♥ ♥ ♥ ♥ 
+♥ ♥   ♥ ♥ ♥ ♥ ♥ ♥ ♥ 
+♥   ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ 
+  ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ 
+'''
+
+#now make the lines go both ways!
+'''
+  ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥   
+♥   ♥ ♥ ♥ ♥ ♥ ♥ ♥   ♥ 
+♥ ♥   ♥ ♥ ♥ ♥ ♥   ♥ ♥ 
+♥ ♥ ♥   ♥ ♥ ♥   ♥ ♥ ♥ 
+♥ ♥ ♥ ♥   ♥   ♥ ♥ ♥ ♥ 
+♥ ♥ ♥ ♥ ♥   ♥ ♥ ♥ ♥ ♥ 
+♥ ♥ ♥ ♥   ♥   ♥ ♥ ♥ ♥ 
+♥ ♥ ♥   ♥ ♥ ♥   ♥ ♥ ♥ 
+♥ ♥   ♥ ♥ ♥ ♥ ♥   ♥ ♥ 
+♥   ♥ ♥ ♥ ♥ ♥ ♥ ♥   ♥ 
+  ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥  
+'''
+
