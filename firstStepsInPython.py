a09942a60a9b2dade80049b81ea7f37e5e6b87be
Constance L Davis <71113829+conDavis@users.noreply.github.com>
2020-10-29 10:07:48 -0400

    Create firstStepsInPython.py

---
+++ firstStepsInPython.py
@@ -0,0 +1,21 @@
+# Welcome to Python!
+
+scaryString = "boo!"
+scared = False
+
+#Scary Script
+print("boo!")
+
+#Really Scary Script
+print("boo!")
+print("boo!")
+print("boo!")
+
+for x in range(3) : 
+  print("boo!")
+
+# A Conditionally Scary Script
+if scared :
+  print("sorry!")
+else : 
+  print("boo!")
